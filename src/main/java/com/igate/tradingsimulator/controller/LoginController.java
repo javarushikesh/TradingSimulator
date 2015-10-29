5aff045c6c083a0b1b90f5f4669fd2403a1d1438
Rushikesh Teli <javarushikesh@gmail.com>
2015-10-29 14:31:28 +0530

    Removed unwanted code

--- src/main/java/com/igate/tradingsimulator/controller/LoginController.java
+++ src/main/java/com/igate/tradingsimulator/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.igate.tradingsimulator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class LoginController {
@@ -14,6 +15,15 @@ public class LoginController {
 	@RequestMapping("/signup")
 	public String signup() {
  		return "signup";
-	}	
+	}
+	
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		System.out.println(message);
+		return new ModelAndView("welcome", "message", message);
+	}
 }
 
